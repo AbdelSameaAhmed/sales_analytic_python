0b047c4bc9c18ed45611a4aef93b7f920735cc10
AbdelSameaAhmed <40595100+AbdelSameaAhmed@users.noreply.github.com>
2023-12-24 15:14:14 +0200

    Update main.py

--- sales_app/main.py
+++ sales_app/main.py
@@ -23,10 +23,7 @@ st.set_page_config(
 
 st.markdown("""
     <style>
-        body 
-        {
-            background_color:black;
-        }
+
         svg
             {
                visibility:hidden;
